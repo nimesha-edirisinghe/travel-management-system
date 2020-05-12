3c7203685252f9751b853c6e48fe1a692850dc21
nimesha-edirisinghe <nimesha@linearsquared.com>
2020-05-12 23:53:50 +0530

    nav bar component v1

--- src/app/components/MyInputWithButton/MyInputWithButton.tsx
+++ src/app/components/MyInputWithButton/MyInputWithButton.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import './MyInputWithButton.scss';
 
-const MyInputWithButton = () => {
+type Props = {
+  placeholder: string;
+  btnLabel: string;
+};
+
+const MyInputWithButton = ({ placeholder, btnLabel }: Props) => {
   return (
-    <div>
-      MyInputWithButton
+    <div className="input-btn-root">
+      <input className="input-field-wrapper" placeholder={placeholder} />
+      <div className="input-btn-wrapper">{btnLabel}</div>
     </div>
   );
-}
+};
+
+MyInputWithButton.defaultProps = {
+  placeholder: 'Enter your Email',
+  btnLabel: 'GO',
+};
 
 export default MyInputWithButton;
-  
