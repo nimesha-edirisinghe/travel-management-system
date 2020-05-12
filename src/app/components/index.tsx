00e5a7bbccee45a97cc87775e32f8e8f5cc2a239
nimesha-edirisinghe <nimesha@linearsquared.com>
2020-05-12 14:59:12 +0530

    implement footer component v1

--- src/app/components/index.tsx
+++ src/app/components/index.tsx
@@ -3,5 +3,13 @@ import MyButton from './MyButton';
 import MyNavBar from './MyNavBar';
 import MyInputWithIcon from './MyInputWithIcon';
 import MyDatePicker from './MyDatePicker';
+import MyInputWithButton from './MyInputWithButton';
 
-export { MyCarousel, MyButton, MyNavBar, MyInputWithIcon, MyDatePicker };
+export {
+  MyCarousel,
+  MyButton,
+  MyNavBar,
+  MyInputWithIcon,
+  MyDatePicker,
+  MyInputWithButton,
+};
