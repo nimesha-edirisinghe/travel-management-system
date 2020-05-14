3b2281ddca24c00e23ea254fa8b10ae5f9d8fb0d
nimesha-edirisinghe <nimesha@linearsquared.com>
2020-05-14 22:44:03 +0530

    navbar component implementation v2

--- src/app/components/MyNavBar/MyNavBar.tsx
+++ src/app/components/MyNavBar/MyNavBar.tsx
@@ -1,15 +1,42 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPhoneVolume } from '@fortawesome/free-solid-svg-icons';
 import './MyNavBar.scss';
 
 const MyNavBar = () => {
   return (
     <div className="navbar">
-      <div className="navbar-top-root">x</div>
+      <div className="navbar-top-root">
+        <div className="navbar-top-root-inner-wrapper">
+          <div className="navbar-top-reg-wrapper">
+            <div className="navbar-social-media-wrapper">
+              <div className="social-media-inner-wrapper" />
+              <div className="social-media-inner-wrapper">x</div>
+              <div className="social-media-inner-wrapper">x</div>
+              <div className="social-media-inner-wrapper">x</div>
+            </div>
+          </div>
+          <div className="navbar-top-reg-wrapper">
+            <div className="logo-wrapper">x</div>
+            <div className="logo-name-wrapper">Samanala Cabs</div>
+          </div>
+          <div className="navbar-top-reg-wrapper">x</div>
+        </div>
+      </div>
       <div className="navbar-bottom-root">
         <div className="navbar-bottom-main-root">
-          <div className="navbar-menu-wrapper">x</div>
+          <div className="navbar-menu-wrapper">
+            <div className="navbar-menu-item">Home</div>
+            <div className="navbar-menu-item">Gallery</div>
+            <div className="navbar-menu-item">About</div>
+            <div className="navbar-menu-item">Contact</div>
+            <div className="navbar-menu-item">Blog</div>
+            <div className="navbar-menu-item">My Account</div>
+          </div>
           <div className="navbar-call-wrapper">
-            <div className="call-icon-wrapper">x</div>
+            <div className="call-icon-wrapper">
+              <FontAwesomeIcon icon={faPhoneVolume} size="4x" color="#FFC000" />
+            </div>
             <div className="call-number-wrapper">
               <div className="call-number-inner-wrapper">033 2226 589</div>
               <div className="call-name-inner-wrapper">Phone line</div>
