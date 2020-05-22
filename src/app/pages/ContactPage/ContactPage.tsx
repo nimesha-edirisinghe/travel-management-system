a650d692100e43c2d20cb3a4743dfc90f25048ee
nimesha-edirisinghe <nimesha@linearsquared.com>
2020-05-22 21:33:34 +0530

    intialize contact page structure

--- src/app/pages/ContactPage/ContactPage.tsx
+++ src/app/pages/ContactPage/ContactPage.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import './ContactPage.scss';
 import { MyNavBar } from 'app/components';
+import FooterSection from '../HomePage/components/FooterSection';
 
 const ContactPage = () => {
   return (
     <div className="contact-page">
       <div className="contact-navbar">
         <MyNavBar />
+      </div>
+      <div className="contact-body-content">
+        <div className="contact-header-wrapper">header</div>
+        <div className="contact-map-wrapper">map</div>
+        <div className="contact-message-wrapper">message</div>
+        <div className="contact-footer-wrapper">
+          <FooterSection />
+        </div>
       </div>
     </div>
   );
