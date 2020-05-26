a0e18714f91a3742bca1851aeba3869505bfa9cb
nimesha-edirisinghe <nimesha@linearsquared.com>
2020-05-26 22:06:45 +0530

    blog page implementation v2

--- src/app/pages/BlogPage/BlogPage.tsx
+++ src/app/pages/BlogPage/BlogPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './BlogPage.scss';
-import { MyNavBar } from 'app/components';
+import { MyNavBar, MyRow, MyCol } from 'app/components';
+import FooterSection from '../HomePage/components/FooterSection';
 
 const BlogPage = () => {
   return (
@@ -9,7 +10,46 @@ const BlogPage = () => {
         <MyNavBar />
       </div>
       <div className="blog-content-wrapper">
-        <div className="blog-inner-wrapper">x</div>
+        <div className="blog-header"> x</div>
+        <div className="blog-inner-wrapper">
+          <div className="blog-inner-content-wrapper">
+            <MyRow gutter={[20, 20]}>
+              <MyCol span={12}>
+                <div className="card-wrapper">x</div>
+              </MyCol>
+              <MyCol span={12}>
+                <div className="card-wrapper">x</div>
+              </MyCol>
+              <MyCol span={12}>
+                <div className="card-wrapper">x</div>
+              </MyCol>
+              <MyCol span={12}>
+                <div className="card-wrapper">x</div>
+              </MyCol>
+              <MyCol span={12}>
+                <div className="card-wrapper">x</div>
+              </MyCol>
+              <MyCol span={12}>
+                <div className="card-wrapper">x</div>
+              </MyCol>
+              <MyCol span={12}>
+                <div className="card-wrapper">x</div>
+              </MyCol>
+              <MyCol span={12}>
+                <div className="card-wrapper">x</div>
+              </MyCol>
+              <MyCol span={12}>
+                <div className="card-wrapper">x</div>
+              </MyCol>
+              <MyCol span={12}>
+                <div className="card-wrapper">x</div>
+              </MyCol>
+            </MyRow>
+          </div>
+        </div>
+        <div className="blog-footer">
+          <FooterSection />
+        </div>
       </div>
     </div>
   );
