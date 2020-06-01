d8956f26a285071edcf269a61d5bb8294c4758cb
nimesha-edirisinghe <nimesha@linearsquared.com>
2020-06-01 22:39:30 +0530

    offer card implementation v2

--- src/app/pages/AboutPage/components/OfferCard/OfferCard.tsx
+++ src/app/pages/AboutPage/components/OfferCard/OfferCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './OfferCard.scss';
+import OfferBlockCard from '../OfferBlockCard';
 
 type Props = {};
 
@@ -9,7 +10,13 @@ const OfferCard = () => {
       <div className="offer-card-inner-root">
         <div className="offer-card-main-title">main title</div>
         <div className="offer-card-sub-title">sub title</div>
-        <div className="offer-card-wrapper">x</div>
+        <div className="offer-card-wrapper">
+          <div className="offer-card-inner-wrapper">
+            <OfferBlockCard />
+          </div>
+          <div className="offer-card-inner-wrapper">x</div>
+          <div className="offer-card-inner-wrapper">x</div>
+        </div>
       </div>
     </div>
   );
