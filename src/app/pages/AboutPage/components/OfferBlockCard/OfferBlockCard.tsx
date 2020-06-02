8ddecd33bb78c100ba08a48d94ea0c39a65e5379
nimesha-edirisinghe <nimesha@linearsquared.com>
2020-06-02 21:48:49 +0530

    finalized offer card implementation

--- src/app/pages/AboutPage/components/OfferBlockCard/OfferBlockCard.tsx
+++ src/app/pages/AboutPage/components/OfferBlockCard/OfferBlockCard.tsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import './OfferBlockCard.scss';
 
-const OfferBlockCard = () => {
+type Props = {
+  imageUrl: string;
+  title: string;
+  description: string;
+};
+
+const OfferBlockCard = ({ imageUrl, title, description }: Props) => {
   return (
     <div className="offer-block-root">
-      <div className="offer-block-image">x</div>
-      <div className="offer-block-main-title">x</div>
-      <div className="offer-block-description">x</div>
-      <div className="offer-block-btn-wrapper">x</div>
+      <div className="offer-block-image">
+        <img src={imageUrl} alt="" height="100%" width="100%" />
+      </div>
+      <div className="offer-block-main-title">{title}</div>
+      <div className="offer-block-description">{description}</div>
+      <div className="offer-block-btn-wrapper">
+        <div className="btn-inner-wrapper">Read more</div>
+      </div>
     </div>
   );
+};
+
+OfferBlockCard.defaultProps = {
+  imageUrl: '',
+  title: '',
+  description: '',
 };
 
 export default OfferBlockCard;
--- src/app/pages/AboutPage/components/OfferCard/OfferCard.tsx
+++ src/app/pages/AboutPage/components/OfferCard/OfferCard.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import './OfferCard.scss';
+import offer1 from 'app/assets/images/offer-1-1.jpg';
+import offer2 from 'app/assets/images/offer-1-2.jpg';
+import offer3 from 'app/assets/images/offer-1-3.jpg';
 import OfferBlockCard from '../OfferBlockCard';
 
 type Props = {};
@@ -8,14 +11,30 @@ const OfferCard = () => {
   return (
     <div className="offer-card-root">
       <div className="offer-card-inner-root">
-        <div className="offer-card-main-title">main title</div>
-        <div className="offer-card-sub-title">sub title</div>
+        <div className="offer-card-main-title">Check out our benefits</div>
+        <div className="offer-card-sub-title">What we’re offering</div>
         <div className="offer-card-wrapper">
           <div className="offer-card-inner-wrapper">
-            <OfferBlockCard />
+            <OfferBlockCard
+              imageUrl={offer1}
+              title="Credit booking"
+              description="There are many van of pasage of suffer alteration lipsum is simply free text."
+            />
           </div>
-          <div className="offer-card-inner-wrapper">x</div>
-          <div className="offer-card-inner-wrapper">x</div>
+          <div className="offer-card-inner-wrapper">
+            <OfferBlockCard
+              imageUrl={offer2}
+              title="Credit booking"
+              description="There are many van of pasage of suffer alteration lipsum is simply free text."
+            />
+          </div>
+          <div className="offer-card-inner-wrapper">
+            <OfferBlockCard
+              imageUrl={offer3}
+              title="Credit booking"
+              description="There are many van of pasage of suffer alteration lipsum is simply free text."
+            />
+          </div>
         </div>
       </div>
     </div>
