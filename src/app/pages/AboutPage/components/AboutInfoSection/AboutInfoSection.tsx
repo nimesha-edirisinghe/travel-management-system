a5e0393fa31ee76ed189f72fcb8077ed45583e95
nimesha-edirisinghe <nimesha@linearsquared.com>
2020-06-06 22:00:35 +0530

    implement about page info section

---
+++ src/app/pages/AboutPage/components/AboutInfoSection/AboutInfoSection.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './AboutInfoSection.scss';
+
+const AboutInfoSection = () => {
+  return (
+    <div className="about-info-root">
+      <div className="about-info-inner-root">
+        <div className="about-info-main-title">title</div>
+        <div className="about-info-body-wrapper">
+          <div className="about-info-body-left-wrapper">
+            <div className="subtitle-wrapper">x</div>
+            <div className="description-wrapper">x</div>
+            <div className="button-wrapper">x</div>
+          </div>
+          <div className="about-info-body-right-wrapper">x</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutInfoSection;
