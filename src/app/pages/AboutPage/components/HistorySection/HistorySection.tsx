5ccfe338f40523a949cd05317dba024dd2b53c2c
nimesha-edirisinghe <nimesha@linearsquared.com>
2020-06-13 23:01:21 +0530

    implement slider component

--- src/app/pages/AboutPage/components/HistorySection/HistorySection.tsx
+++ src/app/pages/AboutPage/components/HistorySection/HistorySection.tsx
@@ -1,13 +1,56 @@
 import React from 'react';
+import Slider from 'react-animated-slider';
+import { ChevronRight, ChevronLeft } from 'react-feather';
+import 'react-animated-slider/build/horizontal.css';
+import image1 from 'app/assets/images/gallery/_gallery-1.jpg';
+import image2 from 'app/assets/images/gallery/_gallery-2.jpg';
+import image3 from 'app/assets/images/gallery/_gallery-3.jpg';
 import './HistorySection.scss';
 
 const HistorySection = () => {
+  const content = [
+    {
+      title: 'title1',
+      path: image1,
+    },
+    {
+      title: 'title2',
+      path: image2,
+    },
+    {
+      title: 'title3',
+      path: image3,
+    },
+  ];
   return (
     <div className="history-section-root">
       <div className="history-section-inner-root">
         <div className="history-section-title">Samanala Cabs History</div>
         <div className="history-section-body-wrapper">
-          <div className="history-image-wrapper">x</div>
+          <div className="history-image-wrapper">
+            <Slider
+              infinite={false}
+              previousButton={<ChevronLeft size={30} color="#ffffff" />}
+              nextButton={<ChevronRight size={30} color="#ffffff" />}
+            >
+              {content.map((item, index) => (
+                <div
+                  key={item.title}
+                  style={{
+                    height: '110%',
+                    width: '100%',
+                  }}
+                >
+                  <img
+                    src={item.path}
+                    alt={item.title}
+                    height="100%"
+                    width="100%"
+                  />
+                </div>
+              ))}
+            </Slider>
+          </div>
           <div className="history-root-wrapper">
             <div className="history-body-title-wrapper">
               How we reached to this level
