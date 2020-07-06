735dded74a65f89cf53a6240ca883c3d8f1df761
nimesha-edirisinghe <nimesha@linearsquared.com>
2020-07-06 21:48:11 +0530

    add rootreducer and root sagas for account

--- src/app/state/rootReducer.tsx
+++ src/app/state/rootReducer.tsx
@@ -3,10 +3,12 @@ import { History } from 'history';
 import { connectRouter } from 'connected-react-router';
 import themeReducer from './theme/themeReducer';
 import blogReducer from './blog/blogListReducer';
+import accountReducer from './account/accountReducer';
 
 export default (history: History) =>
   combineReducers({
     router: connectRouter(history),
     theme: themeReducer,
     blog: blogReducer,
+    account: accountReducer,
   });
--- src/app/state/rootSaga.tsx
+++ src/app/state/rootSaga.tsx
@@ -1,7 +1,8 @@
 import { all } from 'redux-saga/effects';
 import blogSaga from './blog/sagas';
+import accountSaga from './account/sagas';
 import requestSaga from './request/requestSaga';
 
 export default function* rootSaga() {
-  yield all([blogSaga(), requestSaga()]);
+  yield all([blogSaga(), requestSaga(), accountSaga()]);
 }
