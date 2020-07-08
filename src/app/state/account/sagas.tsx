66c3c743dd8a8fb3b43fb30446aa771c6de1281c
nimesha-edirisinghe <nimesha@linearsquared.com>
2020-07-08 20:43:56 +0530

    minor chnages on sagas

--- src/app/state/account/sagas.tsx
+++ src/app/state/account/sagas.tsx
@@ -1,8 +1,9 @@
 import { put, select, take, takeLatest } from 'redux-saga/effects';
 import { MAKE_API_REQUEST, SUCCESS } from '../request/types';
-import { DISPATCH_TOAST_FAILURE } from '../toast/types';
+import { DISPATCH_TOAST_FAILURE, DISPATCH_TOAST_SUCESS } from '../toast/types';
 import { UserListActionTypes, GET_USER_DETAILS } from './types';
 import {} from 'app/api/api';
+import { DISPATCH_TOAST_SUCCESS } from 'app/common/constants';
 
 function* getOfferBuckets(action: UserListActionTypes) {
   try {
@@ -13,6 +14,14 @@ function* getOfferBuckets(action: UserListActionTypes) {
     };
 
     yield put({ type: MAKE_API_REQUEST, payload });
+    const result = yield take(action.type + SUCCESS);
+
+    if (result.payload && result.payload.status === 'success') {
+      yield put({
+        type: DISPATCH_TOAST_SUCCESS,
+        message: 'Successfully fetch',
+      });
+    }
   } catch (e) {
     yield put({ type: DISPATCH_TOAST_FAILURE, message: e.message });
   }
